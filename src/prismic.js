0bc61b08b528ecd094955d0bc589e6fb51312596
mtkwebdev <mtkdev1.0@gmail.com>
2022-01-24 17:47:57 +0000

    prismic cms testing

--- src/prismic.js
+++ src/prismic.js
@@ -2,15 +2,15 @@ import * as prismic from "@prismicio/client"
 import { type } from "@testing-library/user-event/dist/type"
 
 const connect = process.env.REACT_APP_PRISMIC_IO
-export const mkportfolio = 'https://mohammadkawadri.prismic.io/api/v2'
+export const mkportfolio = 'mohammadkawadri'
 export const endpoint = prismic.getEndpoint(mkportfolio)
 
 export const client = prismic.createClient(endpoint, {
     accessToken: connect,
     routes: [
         {
-            type: 'page',
-            path: '/:uid'
+            type: 'projects',
+            path: '/project1/:uid'
         }
     ]
 })
