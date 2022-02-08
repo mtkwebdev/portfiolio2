0f9cffbd703e60e27a7405fa1ac198ff7874e67f
mtkwebdev <mtkdev1.0@gmail.com>
2022-02-08 19:08:06 +0000

    added gradient mesh animation, and backup design

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-import {PrismicRichText, useFirstPrismicDocument, usePrismicDocumentByUID} from '@prismicio/react'
-
+import MainPage from "./pages/MainPage";
 
 function App() {
-  // const  [document] = useFirstPrismicDocument()
-  const  [projectsSection] = usePrismicDocumentByUID('projects','projects')
+
   return (
-    <div className="App">
-    {projectsSection && (   
-      <div>
-      <h1>
-        {projectsSection.data.projects_title[0].text}
-      </h1>
-      <div>
-        {projectsSection.data.repeatable_project[0].project_title[0].text}
-      </div>
-        {projectsSection.data.repeatable_project[0].project_description[0].text}
-      </div>
-    )}
-    {console.log(
-      projectsSection  
-    )}
-    </div>
-  );
+    <>
+    <MainPage/>
+    </>
+  )
+
 }
 
 export default App;
