dab6fccbf45b2f6dccbda2871a9f7f5f4bc31124
mtkwebdev <mtkdev1.0@gmail.com>
2022-02-16 10:23:27 +0000

    cleaned up components, added video component

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Video from '../components/SharedComponents/Backgrounds/Video'
+import contactVideo from '../static/videoDrafts/contact.mp4'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <>
+      <Video src={contactVideo}/>
+    </>
   )
 }
 
--- src/pages/Cv.jsx
+++ src/pages/Cv.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Video from '../components/SharedComponents/Backgrounds/Video'
+import cvVideo from '../static/videoDrafts/CV.mp4'
 
 function Cv() {
   return (
-    <div>Cv</div>
+    <>
+      <Video src={cvVideo}/>
+    </>
   )
 }
 
